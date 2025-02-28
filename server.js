19765dcfb96a65fb493e2d9d0a5a6b371690fea1
ISK-wq <rtyu8028@gmail.com>
2025-02-28 18:51:50 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http') // Чтобы использовать HTTP-интерфейсы в Node.js
+const fs = require('fs') // Для взаимодействия с файловой системой
+const path = require('path') // Для работы с путями файлов и каталогов
+const url = require('url') // Для разрешения и разбора URL 
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    let extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
